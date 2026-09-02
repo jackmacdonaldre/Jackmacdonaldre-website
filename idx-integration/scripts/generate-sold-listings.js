85dd9dc1831a379d9762702967804b5903325e65
jackmacdonaldre <jack@macdonaldgroupre.com>
2026-09-02 16:18:50 -0700

    Refactor sold listings generation script

--- idx-integration/scripts/generate-sold-listings.js
+++ idx-integration/scripts/generate-sold-listings.js
@@ -9,13 +9,20 @@ if (!IDX_ACCESS_KEY) {
 }
 
 const TEAM_AGENT_IDS = ['1186410', '121277198'];
+const CHUNK_HOURS = 720;
+const CHUNK_COUNT = 60;
 
-(async () => {
-  var now = new Date();
-    var pad = function (v) { return String(v).padStart(2, '0'); };
-    var startDatetime = now.getUTCFullYear() + '-' + pad(now.getUTCMonth() + 1) + '-' + pad(now.getUTCDate()) + '+' + pad(now.getUTCHours()) + ':' + pad(now.getUTCMinutes()) + ':' + pad(now.getUTCSeconds());
-    var soldUrl = "https://api.idxbroker.com/clients/soldpending?interval=17520&startDatetime=" + startDatetime + "&dateType=dateAdded";
-  const response = await fetch(soldUrl, {
+function pad(v) {
+  return String(v).padStart(2, '0');
+}
+
+function formatDate(d) {
+  return d.getUTCFullYear() + '-' + pad(d.getUTCMonth() + 1) + '-' + pad(d.getUTCDate()) + '+' + pad(d.getUTCHours()) + ':' + pad(d.getUTCMinutes()) + ':' + pad(d.getUTCSeconds());
+}
+
+async function fetchChunk(startDatetime) {
+  var url = "https://api.idxbroker.com/clients/soldpending?interval=" + CHUNK_HOURS + "&startDatetime=" + startDatetime + "&dateType=dateAdded";
+  var response = await fetch(url, {
     method: "GET",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
@@ -23,40 +30,60 @@ const TEAM_AGENT_IDS = ['1186410', '121277198'];
       "outputtype": "json",
     },
   });
-
- if (response.status !== 200) {
-   console.error("IDX API returned status", response.status);
-   process.exit(1);
- }
-
- const raw = await response.json();
-
- let listings = [];
+  if (response.status === 204) {
+    return [];
+  }
+  if (response.status !== 200) {
+    console.error("IDX API returned status", response.status, "for", startDatetime);
+    return [];
+  }
+  var raw = await response.json();
+  var listings = [];
   if (raw && raw.data && typeof raw.data === 'object' && !Array.isArray(raw.data)) {
     listings = Object.values(raw.data);
   } else if (Array.isArray(raw)) {
     listings = raw;
   }
+  return listings;
+}
 
- listings = listings.filter(l => {
+(async () => {
+  var allListings = [];
+  var seenIds = {};
+  var cursor = new Date();
+
+ for (var i = 0; i < CHUNK_COUNT; i++) {
+   var startDatetime = formatDate(cursor);
+   var chunkListings = await fetchChunk(startDatetime);
+   chunkListings.forEach(function (l) {
+     var key = (l.idxID || '') + '!' + (l.listingID || l.listingId || JSON.stringify(l).slice(0, 40));
+     if (!seenIds[key]) {
+       seenIds[key] = true;
+       allListings.push(l);
+     }
+   });
+   cursor = new Date(cursor.getTime() - CHUNK_HOURS * 60 * 60 * 1000);
+ }
+
+ var listings = allListings.filter(function (l) {
    var status = (l.propStatus || l.idxStatus || '').toString().toLowerCase();
    var agentMatch = TEAM_AGENT_IDS.indexOf(String(l.listingAgentID)) !== -1;
    return status.indexOf('sold') !== -1 && agentMatch;
  });
 
- listings.sort((a, b) => {
+ listings.sort(function (a, b) {
    var da = new Date(a.pendingDate || a.closeDate || a.soldDate || a.dateAdded || 0).getTime();
    var db = new Date(b.pendingDate || b.closeDate || b.soldDate || b.dateAdded || 0).getTime();
    return db - da;
  });
 
- const output = {
+ var output = {
    generatedAt: new Date().toISOString(),
    count: listings.length,
    listings: listings,
  };
 
- const outDir = path.join(__dirname, '..', 'data');
+ var outDir = path.join(__dirname, '..', 'data');
   fs.mkdirSync(outDir, { recursive: true });
   fs.writeFileSync(path.join(outDir, 'sold-listings.json'), JSON.stringify(output, null, 2));
 
